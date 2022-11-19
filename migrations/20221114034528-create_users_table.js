8a950e8c22b553582a611ce60a2057df89436db7
DimasPramantya <101824833+DimasPramantya@users.noreply.github.com>
2022-11-19 09:22:20 +0700

    Update 20221114034528-create_users_table.js

--- migrations/20221114034528-create_users_table.js
+++ migrations/20221114034528-create_users_table.js
@@ -20,7 +20,7 @@ module.exports = {
         unique: true
       },
       password: {
-        type: Sequelize.STRING(30),
+        type: Sequelize.STRING(),
         allowNULL: false
       },
       fullName: {
